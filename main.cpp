663d5026f7e54e7f9be02d0f30f6c772315af071
Danila Zelenkov <zelenkovd@litsey24.ru>
2021-03-07 19:11:42 +0300

    All tests passed

--- main.cpp
+++ main.cpp
@@ -4,16 +4,8 @@
 
 using namespace std;
 
-void swap_args(int *lhs, int *rhs) {
-    int k = *lhs;
-    lhs = rhs;
-    rhs = &k;
-    cout << *lhs << " " << *rhs;
-}
+
 
 int main() {
-int n = 10;
-int m = 15;
-swap_args(&n, &m);
-return 0;
+
 }
